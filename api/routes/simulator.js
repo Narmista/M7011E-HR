ee706180446bea7fb54a1d461474c94a58ee1261
Hampusli <hamlin-6@student.ltu.se>
2019-11-11 11:11:54 +0100

    Update simulator.js
    gaussian function

--- api/routes/simulator.js
+++ api/routes/simulator.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+function gaussianRandom(mean,sigma){ //https://gist.github.com/supereggbert/fe5fb7b1fc30609e983b0207ae136707
+  var u = Math.random();
+  return (u%1e-8>5e-9?1:-1)*Math.sqrt(-Math.log(Math.max(1e-9,u)))*sigma+mean;
+}
+
 router.get('/', (req, res, next) => {
 	res.status(200).json({
-		message: 'get'
+		gaussianRandom(10,1);
 	});
 
 });
 
-router.post('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'post'
-	});
 
-});
 
 module.exports = router;
