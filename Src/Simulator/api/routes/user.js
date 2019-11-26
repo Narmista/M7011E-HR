bbd14a63979fb5e4eb3d6e6cbf1412cdd4101d45
Narmista <54981615+Narmista@users.noreply.github.com>
2019-11-26 13:18:17 +0000

    Fixed auth

--- Src/Simulator/api/routes/user.js
+++ Src/Simulator/api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
