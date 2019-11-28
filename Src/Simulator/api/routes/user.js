86d5c423069f76db7fbe301ddd33f4ca31e5868f
Narmista <54981615+Narmista@users.noreply.github.com>
2019-11-28 09:12:56 +0000

    register for prosumer

--- Src/Simulator/api/routes/user.js
+++ Src/Simulator/api/routes/user.js
@@ -24,7 +24,8 @@ router.post('/signup', (req, res, next) => {
             const user = new User({
               _id: new mongoose.Types.ObjectId(),
               username: req.body.username,
-              password: hash
+              password: hash,
+              name: req.body.name,
             });
             user
               .save()
--- Src/Simulator/app.js
+++ Src/Simulator/app.js
@@ -13,8 +13,8 @@ mongoose.connect('mongodb+srv://Narmista:n4ku7cfe@cluster0-zgvcg.mongodb.net/tes
 })
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
