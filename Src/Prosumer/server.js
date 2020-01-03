55c8af71f2618110d3aa72dec0aea3ddbe31c2e2
Narmista <54981615+Narmista@users.noreply.github.com>
2020-01-03 11:36:48 +0000

    Fixed ssl and https

--- Src/Prosumer/server.js
+++ Src/Prosumer/server.js
@@ -1,11 +1,20 @@
+const app = require('express')();
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 
-const app = express();
 app.use('/static', express.static('public'));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/Login.html'));
+//GET home route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Login.html'));
 });
 
-app.listen(7000);
+// we will pass our 'app' to 'https' server
+https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+    passphrase: 'hej123'
+}, app)
+.listen(443);
--- Src/Simulator/server.js
+++ Src/Simulator/server.js
@@ -1,8 +1,14 @@
-const http = require('http');
+const https = require('https');
 const app = require('./app');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
+const server = https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+    passphrase: 'hej123'
+}, app);
 
 server.listen(port);
+
